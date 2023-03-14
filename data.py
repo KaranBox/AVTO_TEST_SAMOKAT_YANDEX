078a7cb8eca6d4cca35f84fb316d82b859f0c1bc
KaranBox <118733917+KaranBox@users.noreply.github.com>
2023-03-14 11:07:54 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,13 @@
+kit_body = {
+    "name": "a"
+}
+
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Караниус",
+    "phone": "+79623805061",
+    "address": "г. Курск, ул. Ленина,  99"
+}
